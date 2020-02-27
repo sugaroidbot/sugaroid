17e6da48db26f3e4104b484e3c18a1efd77ce3ca
Srevin Saju <srevin03@gmail.com>
2020-02-27 23:38:10 +0300

    Update wiki

--- sugaroid/brain/wiki.py
+++ sugaroid/brain/wiki.py
@@ -4,10 +4,6 @@ from chatterbot.logic import LogicAdapter
 from chatterbot.conversation import Statement
 from nltk import word_tokenize
 
-from sugaroid.brain.postprocessor import sigmaSimilarity, difference
-from sugaroid.brain.preprocessors import normalize
-from sklearn.feature_extraction.text import TfidfVectorizer
-
 from sugaroid.google.google import chatbot_query
 
 
@@ -18,7 +14,6 @@ class WikiAdapter(LogicAdapter):
         super().__init__(chatbot, **kwargs)
 
     def can_process(self, statement):
-        """"""
         self.text = word_tokenize(str(statement))
         tagged = nltk.pos_tag(self.text)
         print(tagged)
@@ -36,22 +31,7 @@ class WikiAdapter(LogicAdapter):
 
     def process(self, statement, additional_response_selection_parameters=None):
         # FIXME: This may printout unrelated data for phrase searches
-        """"
-            normalized = normalize(str(statement))
-            question = ["what", "be", "is", "can", "you", "tell", "me", "please"]
-            confidence = 0.85
-            core = difference(normalized, question)
-            wkpage = wikipedia.page(' '.join(core))
-            if wkpage:
-                selected_statement = Statement(
-                    "{} \n\t ~ Wikipedia".format(wkpage.summary))
-            else:
-                selected_statement = Statement(
-                    "Oops, couldn't find it on Wikipedia")
-            selected_statement.confidence = confidence
-
-            return selected_statement
-        """
+        
         count = 0
         for i in self.text:
             if i.lower() == 'what':
@@ -60,6 +40,13 @@ class WikiAdapter(LogicAdapter):
             selected_statement = Statement('Nothing ' * count)
             selected_statement.confidence = 0.99
         else:
-            selected_statement = Statement(chatbot_query(str(statement)))
-            selected_statement.confidence = 0.94
+            response = chatbot_query(str(statement))
+            if response.lower() == 'loading':
+                confidence = 0
+            elif 'RANDOM ERROR' in response.lower():
+                confidence = 0.05
+            else:
+                confidence = 0.95
+            selected_statement = Statement(str(response))
+            selected_statement.confidence = confidence
         return selected_statement
