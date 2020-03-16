888aaccc5ba5bdc02bbf8bf3eca7c249b2e42944
Srevin Saju <srevin03@gmail.com>
2020-03-16 11:18:06 +0300

    Improve formatiing of fun reply

--- sugaroid/brain/fun.py
+++ sugaroid/brain/fun.py
@@ -75,7 +75,8 @@ class FunAdapter(LogicAdapter):
                 parsed = random_response(FUN_ASK_QUESTION).format(str(statement).lower())
             else:
                 prefix, suffix = random_response(FUN_LET_ME_TRY)
-                suffix = suffix.format(random_response(EMOJI_SMILE))
+                suffix = " {}".format(suffix.format(random_response(EMOJI_SMILE)))
+                prefix = "{} ".format(prefix)
                 emotion = Emotion.wink
 
         selected_statement = SugaroidStatement("{pre}{main}{fix}".format(pre=prefix, main=parsed, fix=suffix))
