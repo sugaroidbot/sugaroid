0949d60b2173a8baa785e496caf9d1cc4e86a808
Srevin Saju <srevin03@gmail.com>
2020-03-16 10:38:39 +0300

    Add support for 'Crack a Joke' and One word command Joke

--- sugaroid/brain/joke.py
+++ sugaroid/brain/joke.py
@@ -26,15 +26,10 @@ SOFTWARE.
 """
 
 import pyjokes
-
 from chatterbot.logic import LogicAdapter
-
+from sugaroid.sugaroid import SugaroidStatement
 from sugaroid.brain.ooo import Emotion
 from sugaroid.brain.preprocessors import normalize
-
-
-class SelectedStatement(object):
-    pass
 
 
 class JokeAdapter(LogicAdapter):
@@ -43,15 +38,17 @@ class JokeAdapter(LogicAdapter):
         super().__init__(chatbot, **kwargs)
 
     def can_process(self, statement):
-        normalized = normalize(str(statement))
-        if (('tell' in normalized) or ('say' in normalized)) and ('joke' in normalized):
+        normalized = normalize(str(statement).lower())
+        if (('tell' in normalized) or ('say' in normalized) or ('crack' in normalized)) and ('joke' in normalized):
+            return True
+        elif (len(normalized) == 1) and (self.chatbot.lp.similarity("joke", str(statement).lower()) >= 0.9):
             return True
         else:
             return False
 
     def process(self, statement, additional_response_selection_parameters=None):
         joke = pyjokes.get_joke('en', 'all')
-        selected_statement = SelectedStatement(joke)
+        selected_statement = SugaroidStatement(joke)
         selected_statement.confidence = 0.95
 
         emotion = Emotion.lol
