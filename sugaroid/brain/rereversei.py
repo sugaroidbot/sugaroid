a4d02f2b259f959f35979ca56856bb2ff1983baf
Srevin Saju <srevin03@gmail.com>
2020-03-27 17:27:30 +0300

    Fix port dictionary reverse

--- sugaroid/brain/rereversei.py
+++ sugaroid/brain/rereversei.py
@@ -50,7 +50,7 @@ class ReReverseAdapter(LogicAdapter):
         self.normalized = None
 
     def can_process(self, statement):
-        if self.chatbot.get_global('reverse'):
+        if self.chatbot.get_global('reverse')['enabled']:
             return True
         else:
             return False
