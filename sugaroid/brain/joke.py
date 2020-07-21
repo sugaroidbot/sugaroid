a5d22ee176121d4f0686bdcccfcce81847688f55
Srevin Saju <srevinsaju@sugarlabs.org>
2020-07-21 22:10:50 +0300

    Update Joke library
    Adapter / Entirely copied from https://github.com/pratishrai/doraemon/blob/302a78f8ace4b4675f3cd293dce101ea448b3e13/cogs/fun.py\#L15 Thanks @pratishrai

--- sugaroid/brain/joke.py
+++ sugaroid/brain/joke.py
@@ -24,8 +24,10 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
 """
+import random
 
 import pyjokes
+import requests
 from chatterbot.logic import LogicAdapter
 from sugaroid.sugaroid import SugaroidStatement
 from sugaroid.brain.ooo import Emotion
@@ -52,7 +54,16 @@ class JokeAdapter(LogicAdapter):
             return False
 
     def process(self, statement, additional_response_selection_parameters=None):
-        joke = pyjokes.get_joke('en', 'all')
+        # https://github.com/pratishrai/doraemon/blob/302a78f8ace4b4675f3cd293dce101ea448b3e13/cogs/fun.py#L15
+        url = 'https://icanhazdadjoke.com/'
+        response1 = requests.get(
+            url, headers= {'Accept': 'application/json'}
+        ).json()
+        response2 = requests.get(
+            "https://sv443.net/jokeapi/v2/joke/Miscellaneous,Dark?type=single"
+        ).json()
+        responses = [response1, response2]
+        joke = random.choice(responses)
         selected_statement = SugaroidStatement(joke, chatbot=True)
         selected_statement.confidence = 0.95
 
