828869251946bf73ddde26334049e3b67aad1d18
Srevin Saju <srevinsaju@sugarlabs.org>
2020-07-21 22:11:08 +0300

    Fix spacing

--- sugaroid/brain/joke.py
+++ sugaroid/brain/joke.py
@@ -57,7 +57,7 @@ class JokeAdapter(LogicAdapter):
         # https://github.com/pratishrai/doraemon/blob/302a78f8ace4b4675f3cd293dce101ea448b3e13/cogs/fun.py#L15
         url = 'https://icanhazdadjoke.com/'
         response1 = requests.get(
-            url, headers= {'Accept': 'application/json'}
+            url, headers={'Accept': 'application/json'}
         ).json()
         response2 = requests.get(
             "https://sv443.net/jokeapi/v2/joke/Miscellaneous,Dark?type=single"
