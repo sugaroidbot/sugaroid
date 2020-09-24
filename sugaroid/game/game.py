fb87f36b95ff9268965d56b4d5847b40213b8768
Srevin Saju <srevinsaju@sugarlabs.org>
2020-09-24 14:48:21 +0300

    refactor: BREAKING CHANGE: deprecate freegames

--- sugaroid/game/game.py
+++ sugaroid/game/game.py
@@ -10,11 +10,7 @@ def game_file(name):
     """"
     Return True if filename represents a game.
     """
-    return (
-        name.endswith('.py')
-        and not name.startswith('__')
-        and name != 'utils.py'
-    )
+    return False
 
 
 games = sorted(name[:-3] for name in contents if game_file(name))
