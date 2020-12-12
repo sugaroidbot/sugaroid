ec936ec2efec67026fcbcda411ea45c149670f52
Srevin Saju <srevinsaju@sugarlabs.org>
2020-12-12 20:21:40 +0300

    style: remove unused import VERSION

--- sugaroid/brain/feel.py
+++ sugaroid/brain/feel.py
@@ -33,7 +33,6 @@ from sugaroid.brain.ooo import Emotion
 from sugaroid.brain.postprocessor import random_response
 from sugaroid.brain.preprocessors import normalize, spac_token
 from sugaroid.sugaroid import SugaroidStatement
-from sugaroid.version import VERSION
 
 
 class FeelAdapter(LogicAdapter):
