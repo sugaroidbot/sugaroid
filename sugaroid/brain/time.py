fd5193c32eabfedd83bab698485430ac64bce53b
Srevin Saju <srevinsaju@sugarlabs.org>
2021-01-28 14:51:35 +0300

    feat: add support for multiple timezones

--- sugaroid/brain/time.py
+++ sugaroid/brain/time.py
@@ -32,6 +32,7 @@ from sugaroid.brain.ooo import Emotion
 from sugaroid.brain.postprocessor import random_response
 from sugaroid.brain.preprocessors import normalize, current_time
 from sugaroid.sugaroid import SugaroidStatement
+from datetime import datetime
 
 
 class TimeAdapter(LogicAdapter):
@@ -53,7 +54,90 @@ class TimeAdapter(LogicAdapter):
             return False
 
     def process(self, statement, additional_response_selection_parameters=None):
+
         emotion = Emotion.positive
+        if "at" in self.normalized or "in" in self.normalized:
+            # possibly the person is asking something like
+            # What is the time at Asia/Bahrain
+            # What is the time in Asia/Bahrain
+            if "at" in self.normalized:
+                idx = self.normalized.index("at")
+            else:
+                idx = self.normalized.index("in")
+            try:
+                place = self.normalized[idx + 1]
+            except IndexError:
+                response = "Please provide a place, for which you require time"
+                emotion = Emotion.positive
+                selected_statement = SugaroidStatement(
+                    "{}".format(response), chatbot=True
+                )
+                selected_statement.confidence = 1
+                selected_statement.emotion = emotion
+                return selected_statement
+
+            import pytz
+
+            if "/" in place:
+                region = place.split("/")[0].capitalize()
+                country = place.split("/")[1].capitalize()
+            else:
+                country = place.capitalize()
+                for region in [
+                    "Asia",
+                    "Africa",
+                    "Europe",
+                    "Australia",
+                    "America",
+                    "Antartica",
+                ]:
+                    try:
+                        tz = pytz.timezone(f"{region}/{country}")
+                        now = datetime.now(tz)
+
+                        selected_statement = SugaroidStatement(
+                            "{}".format(now.ctime()), chatbot=True
+                        )
+                        selected_statement.confidence = 1
+                        selected_statement.emotion = emotion
+                        return selected_statement
+
+                    except pytz.exceptions.UnknownTimeZoneError:
+                        # the timezone is invalid
+                        # so skip
+                        continue
+                else:
+                    # we scanned, but didnt succeed
+                    # sed.
+                    selected_statement = SugaroidStatement(
+                        "I apologize. I can't get the time for that place yet",
+                        chatbot=True,
+                    )
+                    selected_statement.confidence = 1
+                    selected_statement.emotion = emotion
+                    return selected_statement
+
+            try:
+                tz = pytz.timezone(f"{region}/{country}")
+                now = datetime.now(tz)
+
+                selected_statement = SugaroidStatement(
+                    "{}".format(now.ctime()), chatbot=True
+                )
+                selected_statement.confidence = 1
+                selected_statement.emotion = emotion
+                return selected_statement
+
+            except pytz.exceptions.UnknownTimeZoneError:
+                # we scanned, but didnt succeed
+                # sed.
+                selected_statement = SugaroidStatement(
+                    "I apologize. I can't get the time for that place yet", chatbot=True
+                )
+                selected_statement.confidence = 1
+                selected_statement.emotion = emotion
+                return selected_statement
+
         hour, minutes = current_time()
         alert = False
         if hour <= 0:
