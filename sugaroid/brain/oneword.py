bd6efa56efcfdf11544d5bdf921b0ee75e002058
Srevin Saju <srevinsaju@sugarlabs.org>
2021-01-30 19:04:26 +0300

    feat: add credits,acknowledgements command

--- sugaroid/brain/oneword.py
+++ sugaroid/brain/oneword.py
@@ -70,7 +70,7 @@ class OneWordAdapter(LogicAdapter):
         short = str(statement).lower()
         if "ver" in short:
             response = VERSION
-            confidence = 0.99
+            confidence = 1
         elif "name" in short:
             response = "What name? You should probably use better english"
 
@@ -86,7 +86,7 @@ class OneWordAdapter(LogicAdapter):
                     ).strip()
                 )
             response = "hmm. Sure. \n {}".format("\n ".join(help_files))
-            confidence = 0.99
+            confidence = 1
 
         elif "help" in short:
             response = (
@@ -94,39 +94,35 @@ class OneWordAdapter(LogicAdapter):
                 "If you are serious of what you are asking, "
                 "type help404"
             )
-            confidence = 0.99
+            confidence = 1
         elif "disclaimer" in short:
             response = DISCLAIMER
-            confidence = 0.99
+            confidence = 1
         elif "license" in short:
-            lic = """```
-                                            MIT License
-
-                                  Sugaroid Artificial Intelligence
-                                            Chatbot Core
-                                   Copyright (c) 2020-2021 Srevin Saju
+            lic = """<pre><code>
+MIT License
+Sugaroid Artificial Intelligence
+Chatbot Core
+Copyright (c) 2020-2021 Srevin Saju
 Copyright (c) 2021 The Sugaroid Project
 
-            Permission is hereby granted, free of charge, to any person obtaining a copy
-            of this software and associated documentation files (the "Software"), to deal
-            in the Software without restriction, including without limitation the rights
-            to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-            copies of the Software, and to permit persons to whom the Software is
-            furnished to do so, subject to the following conditions:
-
-            The above copyright notice and this permission notice shall be included in all
-            copies or substantial portions of the Software.
-
-            THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-            IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-            FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-            AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-            LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-            OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-            SOFTWARE.```
-            """
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.</code></pre>"""
             response = lic
-            confidence = 0.99
+            confidence = 1
+
+        elif "credit" in short or "acknow" in short or "people" in short:
+            credits = [
+                "<b>The Sugaroid Project</b>, and contributors",
+                "<b>Wolfram Alpha</b> for Mathematical evaluation and logical"
+                " wh-questions",
+                "<b>The Swaglyrics Project</b>, for the awesome lyrics fetching library",
+                "<b>The NewsAPI Project</b>, for the news headlines",
+                "<b>Wikipedia</b>, The Free Encyclopedia, by The Wikimedia Foundation",
+            ]
+            response = '❇️ ' + '\n ❇️ '.join(credits)
+            confidence = 1
+
         elif short in HI_WORDS:
             response = random_response(HI_RESPONSES)
             confidence = 0.99
