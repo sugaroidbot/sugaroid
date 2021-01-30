8e3497b4fce9aea2a0f45bcc0087521798adb29b
Srevin Saju <srevinsaju@sugarlabs.org>
2021-01-30 21:22:49 +0300

    fix: use swaglyrics' internal get_lyrics method to get the lyrics of the author instead of a custom implementation

--- sugaroid/brain/swaglyrics.py
+++ sugaroid/brain/swaglyrics.py
@@ -27,9 +27,8 @@ SOFTWARE.
 """
 import re
 import requests
-from swaglyrics.cli import stripper
+from swaglyrics.cli import stripper, get_lyrics
 from chatterbot.logic import LogicAdapter
-from bs4 import BeautifulSoup, UnicodeDammit
 from sugaroid.sugaroid import SugaroidStatement
 from sugaroid.brain.ooo import Emotion
 from sugaroid.brain.preprocessors import normalize
@@ -51,53 +50,6 @@ def fetch(url, **kwargs):
     }
     req = requests.get(url, headers=headers, **kwargs)
     return req.text
-
-
-def get_lyrics(song, artist):
-    """
-    doc: https://github.com/SwagLyrics/SwagLyrics-discord-bot/blob/3922b4ff8ca1d758d48731eb4f4c5aee43676edc/SwagLyricsBot/lyrics.py#L22
-    doc: https://github.com/SwagLyrics/SwagLyrics-For-Spotify/blob/master/swaglyrics/cli.py#L75
-    Get lyrics from Genius given the song and artist.
-    Formats the URL with the stripped url path to fetch the lyrics.
-    :param song: currently playing song
-    :param artist: song artist
-    :return: song lyrics or None if lyrics unavailable
-    """
-    url_data = stripper(song, artist)  # generate url path using stripper()
-    if url_data.startswith("-") or url_data.endswith("-"):
-        # url path had either song in non-latin, artist in non-latin, or both
-        raise LyricsNotFound(f"Lyrics for {song} by {artist} not found on Genius.")
-    url = f"https://genius.com/{url_data}-lyrics"  # format the url with the url path
-
-    try:
-        page = fetch(url)
-    except requests.exceptions.HTTPError:
-        url_data = fetch(
-            f"{backend_url}/stripper", data={"song": song, "artist": artist}
-        )
-        if not url_data:
-            raise LyricsNotFound(f"Lyrics for {song} by {artist} not found on Genius.")
-        url = f"https://genius.com/{url_data}-lyrics"
-        page = fetch(url)
-    html = BeautifulSoup(page, "html.parser")
-    lyrics_path = html.find(
-        "div", class_="lyrics"
-    )  # finding div on Genius containing the lyrics
-    if lyrics_path:
-        lyrics = UnicodeDammit(lyrics_path.get_text().strip()).unicode_markup
-    else:
-        # hotfix!
-        lyrics_path = html.find_all("div", class_=re.compile("^Lyrics__Container"))
-        lyrics_data = []
-        for x in lyrics_path:
-            lyrics_data.append(
-                UnicodeDammit(
-                    re.sub("<.*?>", "", str(x).replace("<br/>", "\n"))
-                ).unicode_markup
-            )
-
-        lyrics = "\n".join(lyrics_data)
-    return lyrics
 
 
 class SwagLyricsAdapter(LogicAdapter):
@@ -141,7 +93,7 @@ class SwagLyricsAdapter(LogicAdapter):
 
         try:
             lyrics = get_lyrics(song, artist)
-            if not lyrics.strip():
+            if not lyrics or not lyrics.strip():
                 raise LyricsNotFound
 
         except LyricsNotFound:
