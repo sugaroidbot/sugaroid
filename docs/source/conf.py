244a86f852eb64319a43e594ac6cd7506f9f90b7
Srevin Saju <srevinsaju@sugarlabs.org>
2021-01-31 21:27:10 +0300

    feat: lint docs/conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,23 +12,31 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+
+sys.path.insert(0, os.path.abspath(".."))
 import sphinx_rtd_theme
 import mock
+
 # 'chatterbot', 'chatterbot.logic',
-MOCK_MODULES = [ 'chatterbot_corpus', 'chatterbot_corpus.corpus',
-                'launcher', 'chatterbot.trainers', 'googlesearch', 'setup']
+MOCK_MODULES = [
+    "chatterbot_corpus",
+    "chatterbot_corpus.corpus",
+    "launcher",
+    "chatterbot.trainers",
+    "googlesearch",
+    "setup",
+]
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
 # -- Project information -----------------------------------------------------
 
-project = 'Sugaroid'
-copyright = '2021, Srevin Saju, The Sugaroid Project'
-author = 'Srevin Saju, The Sugaroid Project'
+project = "Sugaroid"
+copyright = "2021, Srevin Saju, The Sugaroid Project"
+author = "Srevin Saju, The Sugaroid Project"
 
 # The full version, including alpha/beta/rc tags
-release = 'v0.15.2'
+release = "v0.15.2"
 
 
 # -- General configuration ---------------------------------------------------
@@ -36,12 +44,10 @@ release = 'v0.15.2'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    'sphinx.ext.todo', 'sphinx.ext.viewcode', 'sphinx.ext.autodoc'
-]
+extensions = ["sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.autodoc"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -54,12 +60,12 @@ exclude_patterns = ["../../venv", "launcher.py", "setup.py"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 html_theme_options = {
-    'style_nav_header_background': '#ff004a',
+    "style_nav_header_background": "#ff004a",
 }
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
