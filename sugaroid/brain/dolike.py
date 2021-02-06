7585bc808c765499d9c44dd52d66d82179e8adcc
sreyasaju <sreyasaju258@gmail.com>
2021-02-06 13:52:38 +0300

    Add more responses to dolike.py

--- sugaroid/brain/dolike.py
+++ sugaroid/brain/dolike.py
@@ -46,6 +46,9 @@ SUGAROID_LIKES = {
     "fruit": "I like strawberries even though I haven't tasted one yet ",
     "color": "I like blue color the most",
     "subject": "I'm fond of computer science and my developer likes it a lot. ",
+    "music":"I listen to the rotating CPU fan. Its a harmonic music! ",
+    "actor":"I do not watch movies, so yea!",
+    "comedian":"My favorite comedian is Mr Bean",
     "education": "Yes, I like it a lot. Maybe you don't. But that's when I become smarter to answer your questions",
 }
 
