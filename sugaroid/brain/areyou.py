ec13c3a7e160d138cf1ab0002a8038cfe99ad039
Srevin Saju <srevinsaju@sugarlabs.org>
2021-02-08 11:50:40 +0300

    feat: use sugaroid.SugaroidStatement for areyou.py

--- sugaroid/brain/areyou.py
+++ sugaroid/brain/areyou.py
@@ -4,7 +4,7 @@ from sugaroid.brain.constants import WHO_AM_I, WHO_ARE_YOU, SUGAROID
 from sugaroid.brain.ooo import Emotion
 from sugaroid.brain.postprocessor import random_response
 from sugaroid.brain.preprocessors import normalize
-from sugaroid.sugaroid import SugaroidStatement
+from sugaroid.core.statement import SugaroidStatement
 from sugaroid.version import VERSION
 
 
