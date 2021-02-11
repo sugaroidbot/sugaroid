db329580da2ebe573d30849a800c4084d7b68d88
Srevin Saju <srevinsaju@sugarlabs.org>
2021-02-11 09:59:07 +0300

    fix: joke adapter
    Co-authored-by: Joel Anil Chacko <joelanilchacko@gmail.com>

--- sugaroid/brain/joke.py
+++ sugaroid/brain/joke.py
@@ -32,14 +32,15 @@ class JokeAdapter(LogicAdapter):
             return False
 
     def process(self, statement, additional_response_selection_parameters=None):
-        # https://github.com/pratishrai/doraemon/blob/302a78f8ace4b4675f3cd293dce101ea448b3e13/cogs/fun.py#L15
-        url = "https://icanhazdadjoke.com/"
-        response1 = requests.get(url, headers={"Accept": "application/json"}).json()
-        response2 = requests.get(
-            "https://sv443.net/jokeapi/v2/joke/Miscellaneous,Dark?type=single"
-        ).json()
-        responses = [response1, response2]
-        joke = random.choice(responses)
+        # https://github.com/pratishrai/doraemon/blob/302a78f8ace4b4675f3cd293dce101ea448b3e13/cogs/fun.py#L1
+        try:
+            response2 = requests.get(
+                "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit"
+            ).json()
+            joke = response2["joke"]
+        except Exception as e:
+            joke = f"I think I am a joke sometimes.. {e}"
+
         selected_statement = SugaroidStatement(joke, chatbot=True)
         selected_statement.confidence = 0.95
 
