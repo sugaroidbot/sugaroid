f5467bcbd993a933f4d4f620d31a6a24303f8aff
Srevin Saju <srevinsaju@sugarlabs.org>
2021-02-11 10:55:19 +0300

    feat: remove unused imports and port to SugaroidLogicAdapter

--- sugaroid/brain/joke.py
+++ sugaroid/brain/joke.py
@@ -1,49 +1,53 @@
 import random
 
-import pyjokes
 import requests
 from chatterbot.logic import LogicAdapter
+
+from sugaroid.core.base_adapters import SugaroidLogicAdapter
 from sugaroid.sugaroid import SugaroidStatement
 from sugaroid.brain.ooo import Emotion
 from sugaroid.brain.preprocessors import normalize
 
 
-class JokeAdapter(LogicAdapter):
+class JokeAdapter(SugaroidLogicAdapter):
     """
-    Gets a random joke from the Chuck Norris Database
+    Gets a random joke from the v2.joke api
     """
 
-    def __init__(self, chatbot, **kwargs):
-        super().__init__(chatbot, **kwargs)
-
-    def can_process(self, statement):
-        normalized = normalize(str(statement).lower())
+    def can_process(self, statement: SugaroidStatement) -> bool:
         if (
-            ("tell" in normalized) or ("say" in normalized) or ("crack" in normalized)
-        ) and ("joke" in normalized):
+            ("tell" in statement.lemma)
+            or ("say" in statement.lemma)
+            or ("crack" in statement.lemma)
+        ) and ("joke" in statement.lemma):
             return True
-        elif (len(normalized) == 1) and (
-            self.chatbot.lp.similarity("joke", str(statement).lower()) >= 0.9
+        elif (len(statement.lemma) == 1) and (
+            self.chatbot.lp.similarity("joke", statement.text) >= 0.9
         ):
             return True
-        elif "joke" in normalized:
+        elif "joke" in statement.lemma:
             return True
-        else:
-            return False
+        return False
 
-    def process(self, statement, additional_response_selection_parameters=None):
+    def process(self, statement: SugaroidStatement, additional_response_selection_parameters=None):
         # https://github.com/pratishrai/doraemon/blob/302a78f8ace4b4675f3cd293dce101ea448b3e13/cogs/fun.py#L1
         try:
             response2 = requests.get(
                 "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit"
             ).json()
-            joke = response2["joke"]
+            try:
+                joke = response2["joke"]
+            except KeyError:
+                setup = response2["setup"]
+                delivery = response2["delivery"]
+                if self.chatbot.globals["rich"]:
+                    joke = f"{setup}\n\n<i>{delivery}</i>"
+                else:
+                    joke = f"{setup}\n\n{delivery}"
         except Exception as e:
             joke = f"I think I am a joke sometimes.. {e}"
 
         selected_statement = SugaroidStatement(joke, chatbot=True)
-        selected_statement.confidence = 0.95
-
-        emotion = Emotion.lol
-        selected_statement.emotion = emotion
+        selected_statement.set_confidence(1)
+        selected_statement.set_emotion(Emotion.lol)
         return selected_statement
