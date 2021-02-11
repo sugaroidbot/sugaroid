sugaroidbot/sugaroid

commit 1cd5b29c7f2595a79f0adc3a2271c54df32c2153

fix: apply suggested changes

sugaroid/brain/wiki.py
@@ -61,7 +61,7 @@ class WikiAdapter(LogicAdapter):
             selected_statement.confidence = 1.0
             selected_statement.emotion = emotion
             return selected_statement
-        elif ("Up" in self.text) or ("up" in self.text):
+        elif "up" in self.text.lower():
             response = random_response(WHATSUP)
             selected_statement = SugaroidStatement(response, chatbot=True)
             selected_statement.confidence = 1.0

commit 4470cce7f96df89e9427bd9c6c465993165995f0

fix: regression introduced by https://github.com/sugaroidbot/sugaroid/commit/1cd5b29c7f2595a79f0adc3a2271c54df32c2153

sugaroid/brain/wiki.py
@@ -61,12 +61,12 @@ class WikiAdapter(LogicAdapter):
             selected_statement.confidence = 1.0
             selected_statement.emotion = emotion
             return selected_statement
-        elif "up" in self.text.lower():
+        """elif "up" in self.text.lower():
             response = random_response(WHATSUP)
             selected_statement = SugaroidStatement(response, chatbot=True)
             selected_statement.confidence = 1.0
             selected_statement.emotion = emotion
-            return selected_statement
+            return selected_statement"""
 
         else:
             nlp = self.chatbot.lp