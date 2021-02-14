2998f8a3e3070630ed6c7255c89ac297b24c9cf3
Soul-Breaker <78663252+Soul-Breaker@users.noreply.github.com>
2021-02-14 23:20:06 +0300

    Update hangman.py

--- sugaroid/brain/hangman.py
+++ sugaroid/brain/hangman.py
@@ -222,7 +222,7 @@ zodiac
 )[
     1:-1
 ]
-HANGAMN_MARVEL_WORDS="""
+HANGAMaN_MARVEL_WORDS="""
 captainamerica 
 thor 
 hawkeye 
@@ -322,6 +322,121 @@ HANGMAN_STICKFIGURE=["""
     ========= 
 """]
 
+def Marvel_fans():
+    def Random(wordList):
+        wordIndex = random.randint(0,len(wordList) - 1)            
+        return wordList[wordIndex]
+    def game(HANGMANPICS,missedLetters,correctLetters,secretWord):
+        print(HANGMANPICS[len(missedLetters)])
+        print()
+
+        print('Missed letters:',end = '')
+        for letter in missedLetters:
+            print(letter,end ='')
+        print()
+
+        blanks ='_' * len(secretWord)
+
+        for i in range(len(secretWord)):            
+             if secretWord[i] in correctLetters:
+                 blanks = blanks[:i] + secretWord[i] + blanks[i+1:]
+
+        for letter in blanks:       
+            print(letter,end=' ')
+        print()
+    def Guess(alreadyGuessed):
+
+         while True:
+
+             print('Guess a letter.')
+             guess = input()
+
+             guess = guess.lower()
+
+             if len(guess) != 1:
+
+                 print('Please enter a single letter.')
+
+             elif guess in alreadyGuessed:
+
+                 print('You have already guessed that letter. Choose again.')
+
+             elif guess not in 'abcdefghijklmnopqrstuvwxyz':
+
+                 print('Invalid Entry! Please enter a LETTER.')
+
+             else:
+                 return guess
+
+    def play_Again():
+
+         print('Do you want to play again?(yes/no)')
+
+         return input().lower().startswith('y')
+
+
+    print('H A N G M A N')
+
+    missedLetters=''
+
+    correctLetters=''
+
+    secretWord=Random(HANGMAN_MARVEL_WORDS)
+
+    game_done=False
+
+
+     while True:
+
+         game(HANGMANPICS, missedLetters, correctLetters, secretWord)
+
+
+         guess = Guess(missedLetters + correctLetters)
+
+
+         if guess in secretWord:
+
+             correctLetters = correctLetters + guess
+             foundAllLetters = True
+
+             for i in range(len(secretWord)):
+
+                 if secretWord[i] not in correctLetters:
+
+                     foundAllLetters = False
+
+                     break
+
+             if foundAllLetters : 
+
+                 print('Yes! The secret word is "' + secretWord + '"! You have won!')
+
+                 game_done = True
+
+         else:
+
+             missedLetters = missedLetters + guess
+
+             if len(missedLetters) == len(HANGMANPICS) - 1:
+
+                 game(HANGMANPICS, missedLetters, correctLetters, secretWord)
+
+                 print('You have run out of guesses!\nAfter ' + str(len(missedLetters)) + ' missed guesses and ' + str(len(correctLetters)) + ' correct guesses, the word was "' + secretWord + '"')
+
+                 game_done = True
+
+
+         if game_done:
+
+             if play_Again():
+                 missedLetters=''
+                 correctLetters=''
+                 game_done=False
+                 secretWord=Random(HANMAN_MARVEL_WORDS)
+
+             else:
+                break
+                
 HANGMAN_WIN = [
     "Hurray! You won the game!",
     "Lets celebrate. You deserve the win",
