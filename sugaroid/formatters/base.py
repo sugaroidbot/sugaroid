2b18506caf9ae5b6c9112c3e54bed20dd3223fc5
Srevin Saju <srevinsaju@sugarlabs.org>
2021-02-18 00:00:00 +0300

    feat: improve base formatters

---
+++ sugaroid/formatters/base.py
@@ -0,0 +1,11 @@
+
+
+class BaseFormatter:
+    def __init__(self, left: str = None, right: str = None, sub_left: str = None, sub_right: str = None):
+        self.left = left
+        self.right = right
+        self.sub_left = sub_left
+        self.sub_right = sub_right
+
+
+
