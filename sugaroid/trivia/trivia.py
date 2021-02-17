1e0941c0767a43b5963195034e3a192d60ec3ff4
Srevin Saju <srevinsaju@sugarlabs.org>
2021-02-18 00:00:24 +0300

    feat: improve trivia adapter

--- sugaroid/trivia/trivia.py
+++ sugaroid/trivia/trivia.py
@@ -1,5 +1,7 @@
-from random import randint
-from sugaroid.trivia.triviadb import db
+from random import shuffle
+
+import requests
+
 
 
 class SugaroidTrivia:
@@ -12,22 +14,47 @@ class SugaroidTrivia:
         >>> strivia = SugaroidTrivia()
         >>> print(strivia.ask())
         >>> # get the answer
-        >>> strivia.answer()
+        >>> strivia.ch()
 
     """
 
     def __init__(self):
         self.answer_i = None
-        self.random_question_instance = db[randint(0, 50)]
+        self._correct_answer_idx = 0
 
-    def ask(self):
-        category = self.random_question_instance["category"]
-        difficulty = self.random_question_instance["difficulty"]
-        question = self.random_question_instance["question"]
-        self.answer_i = self.random_question_instance["correct_answer"]
-        return "Category: {c}\nDifficulty: {d}\n\n{q}".format(
-            c=category, d=difficulty, q=question
-        )
+        response = requests.get("https://opentdb.com/api.php?amount=1&type=multiple")
+        self._response = response.json()
 
-    def answer(self):
-        return self.answer_i
+        # results parsed
+        results = self._response["results"][0]
+        self.category = results["category"]
+        self.difficulty = results["difficulty"]
+        self.question = results["question"]
+        self.correct_answer = results["correct_answer"]
+        self.incorrect_answers = results["incorrect_answers"]
+        self._options = None
+
+    def ask(self) -> str:
+        options = [self.correct_answer] + self.incorrect_answers
+        shuffle(options)
+
+        self._correct_answer_idx = options.index(self.correct_answer)
+        self._options = options
+        return """Difficulty: {difficulty}
+        
+Q: {question}
+
+Options
+a. {option_a}
+b. {option_b}
+c. {option_c}
+d. {option_d}""".format(difficulty=self.difficulty, question=self.question,
+                   option_a=options[0], option_b=options[1], option_c=options[2], option_d=options[3])
+
+    def check_answer(self, answer: str) -> bool:
+        if answer.strip().isdigit():
+            is_correct = answer.strip().isdigit() == str(self._correct_answer_idx)
+        else:
+            is_correct = answer.strip() == self.correct_answer
+        return is_correct
+
