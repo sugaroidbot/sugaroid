967293178595f47b832184e2defad110febb1822
Srevin Saju <srevinsaju@sugarlabs.org>
2021-02-17 23:59:03 +0300

    feat: move all the logic from oneword to commands

--- sugaroid/brain/oneword.py
+++ sugaroid/brain/oneword.py
@@ -32,51 +32,8 @@ class OneWordAdapter(SugaroidLogicAdapter):
         additional_response_selection_parameters=None,
     ) -> SugaroidStatement:
         emotion = Emotion.seriously
-        confidence = 0.60
+        confidence = 0.40
         response = random_response(ONE_WORD)
-        short = str(statement).lower()
-        if "ver" in short:
-            response = VERSION
-            confidence = 1
-        elif "name" in short:
-            response = "What name? You should probably use better english"
-
-        elif ("help404" in short) or ("help" in short and "404" in short):
-            import sugaroid  # noqa:
-            import chatterbot  # noqa:
-
-            help_files = []
-            for i in self.chatbot.globals["adapters"]:
-                help_files.append(
-                    "{}: {}".format(
-                        i.split(".")[-1].strip(), eval(i).__doc__.strip()
-                    ).strip()
-                )
-            response = "hmm. Sure. \n {}".format("\n ".join(help_files))
-            confidence = 1
-
-        elif "help" in short:
-            response = (
-                "The help is not very easily provided. "
-                "If you are serious of what you are asking, "
-                "type help404"
-            )
-            confidence = 1
-        elif "disclaimer" in short:
-            response = DISCLAIMER
-            confidence = 1
-        elif "license" in short:
-            response = LICENSE
-            confidence = 1
-
-        elif "credit" in short or "acknow" in short or "people" in short:
-
-            response = "❇️ " + "\n ❇️ ".join(CREDITS)
-            confidence = 1
-
-        elif short in HI_WORDS:
-            response = random_response(HI_RESPONSES)
-            confidence = 0.99
         selected_statement = SugaroidStatement(response, chatbot=True)
         selected_statement.confidence = confidence
         selected_statement.emotion = emotion
