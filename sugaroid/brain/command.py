913b1b0e11baebff0700f0d4146c921e598bea7a
Srevin Saju <srevinsaju@sugarlabs.org>
2021-02-17 23:58:36 +0300

    feat: add new adapter command.py for Command handling

---
+++ sugaroid/brain/command.py
@@ -0,0 +1,97 @@
+import os
+
+from sugaroid.core.base_adapters import SugaroidLogicAdapter
+from sugaroid.version import VERSION
+from sugaroid.brain.constants import (
+    DISCLAIMER,
+    HI_WORDS,
+    HI_RESPONSES,
+    LICENSE,
+    CREDITS,
+)
+from sugaroid.brain.ooo import Emotion
+from sugaroid.brain.postprocessor import random_response
+from sugaroid.sugaroid import SugaroidStatement
+
+
+class CommandAdapter(SugaroidLogicAdapter):
+    """
+    Adapter for processing commands
+    """
+
+    def can_process(self, statement: SugaroidStatement) -> bool:
+        return len(statement.words) == 1
+
+    def process(
+        self,
+        statement: SugaroidStatement,
+        additional_response_selection_parameters=None,
+    ) -> SugaroidStatement:
+
+        emotion = Emotion.smirking
+        confidence = 1
+        short = statement.words[0]
+
+        if "ver" in short:
+            version = VERSION
+            if not os.getenv("DYNO"):
+                version = f"{version}.dev0 (local build)"
+            response = version
+        elif short == "name":
+            response = "What name? You should probably use better english"
+        elif short == "refresh":
+            self.chatbot.session.refresh()
+            response = "Cleared cached data."
+        elif short == "stats":
+            import psutil
+            process = psutil.Process(os.getpid())
+            megabytes_used = process.memory_info().rss / (10**6)
+            version = VERSION
+            if not os.getenv("DYNO"):
+                version = f"{version}.dev0 (local build)"
+
+            response = f"<b>Up since</b>: {self.chatbot.start_time.isoformat()} \n" \
+                       f"<b>Memory usage</b>: {megabytes_used} " \
+                       f"<b>Version</b>: {version}"
+
+        elif ("help404" in short) or ("help" in short and "404" in short):
+            import sugaroid  # noqa:
+            import chatterbot  # noqa:
+
+            help_files = []
+            for i in self.chatbot.globals["adapters"]:
+                help_files.append(
+                    "{}: {}".format(
+                        i.split(".")[-1].strip(), eval(i).__doc__.strip()
+                    ).strip()
+                )
+            response = "hmm. Sure. \n {}".format("\n ".join(help_files))
+            confidence = 1
+
+        elif "help" in short:
+            response = (
+                "The help is not very easily provided. "
+                "If you are serious of what you are asking, "
+                "type help404"
+            )
+            confidence = 1
+        elif "disclaimer" in short:
+            response = DISCLAIMER
+            confidence = 1
+        elif "license" in short:
+            response = LICENSE
+            confidence = 1
+        elif "credit" in short or "acknow" in short or "people" in short:
+            response = "❇️ " + "\n ❇️ ".join(CREDITS)
+            confidence = 1
+        elif short in HI_WORDS:
+            response = random_response(HI_RESPONSES)
+            confidence = 1
+        else:
+            response = "None"
+            confidence = 0
+        selected_statement = SugaroidStatement(response, chatbot=True)
+        selected_statement.confidence = confidence
+        selected_statement.emotion = emotion
+        selected_statement.adapter = None
+        return selected_statement
